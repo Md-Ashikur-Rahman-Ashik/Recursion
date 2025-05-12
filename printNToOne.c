6a5d5392b230df321db0559ba2c9b3560c55b320
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-05-12 16:21:40 +0600

    Explored printing in reverse order

--- printNToOne.c
+++ printNToOne.c
@@ -1,25 +1,50 @@
+// #include <stdio.h>
+
+// void printNToOne(int num)
+// {
+//     if (num > 1)
+//     {
+//         /* code */
+//         printf("%d\n", num);
+//         printNToOne(num - 1);
+//     }
+//     else if (num == 1)
+//     {
+//         printf("%d", num);
+//     }
+// }
+
+// int main()
+// {
+//     int num;
+//     scanf("%d", &num);
+
+//     printNToOne(num);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-void printNToOne(int num)
+void printInReverse(int num1, int num2)
 {
-    if (num > 1)
+    if (num2 == 6)
     {
         /* code */
-        printf("%d\n", num);
-        printNToOne(num - 1);
+        return;
     }
-    else if (num == 1)
-    {
-        printf("%d", num);
-    }
+    printInReverse(num1, num2 + 1);
+    printf("%d\n", num2);
 }
 
 int main()
 {
-    int num;
-    scanf("%d", &num);
+    int num1;
+    scanf("%d", &num1);
 
-    printNToOne(num);
+    int num2 = 1;
+
+    printInReverse(num1, num2);
 
     return 0;
 }
